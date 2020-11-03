35536e623813aa6d668916af99a4064421ae4553
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2020-11-04 00:01:19 +0100

    Added put and delete actions to HttpService

--- cs-common/src/services/http.service.ts
+++ cs-common/src/services/http.service.ts
@@ -2,41 +2,56 @@ import { ActionResponse } from "..";
 
 export class HttpService {
 
-    public async get<TBody, TRespose>(url: string): Promise<TRespose> {
+    public async get<TRespose>(url: string): Promise<TRespose> {
+        return this.performUrlCall<TRespose>('GET', url);
+    }
+
+    public async post<TBody, TRespose>(url: string, body: TBody): Promise<TRespose> {
+        return this.performBodyCall<TBody, TRespose>('POST', url, body);
+    }
+
+    public async put<TBody, TRespose>(url: string, body: TBody): Promise<TRespose> {
+        return this.performBodyCall<TBody, TRespose>('PUT', url, body);
+    }
+
+    public async delete<TResponse>(url: string): Promise<TResponse> {
+        return this.performUrlCall<TResponse>('DELETE', url);
+    }
+
+
+    private performUrlCall<TRespose>(action: string, url: string): TRespose | PromiseLike<TRespose> {
         return fetch(url, {
-            method: 'GET',
+            method: action,
             headers: {
                 'Accept': `application/json, text/plain, */*`
-            }
-            })
+            }})
             .then(httpRespose => {
                 if (!httpRespose.ok) {
-                    throw new Error(httpRespose.statusText)
+                    throw new Error(httpRespose.statusText);
                 }
                 return httpRespose.json() as Promise<ActionResponse<TRespose>>;
             })
             .then(actionResponse => {
-                return actionResponse.data
+                return actionResponse.data;
             });
     }
-
-    public async post<TBody, TRespose>(url: string, body: TBody): Promise<TRespose> {
+    
+    private performBodyCall<TBody, TRespose>(action: string, url: string, body: TBody): TRespose | PromiseLike<TRespose> {
         return fetch(url, {
-            method: 'POST',
+            method: action,
             body: JSON.stringify(body),
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': `application/json, text/plain, */*`
-            }
-            })
+            }})
             .then(httpRespose => {
                 if (!httpRespose.ok) {
-                    throw new Error(httpRespose.statusText)
+                    throw new Error(httpRespose.statusText);
                 }
                 return httpRespose.json() as Promise<ActionResponse<TRespose>>;
             })
             .then(actionResponse => {
-                return actionResponse.data
+                return actionResponse.data;
             });
     }
 

a667c5abcbabcd2e9e045cce61c4af2ed9009935
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2020-11-23 00:11:52 +0100

    Testing part created with Mocha and Chai

--- cs-common/src/index.ts
+++ cs-common/src/index.ts
@@ -1,5 +1,6 @@
 export { ActionResponseType } from './enums/action-response-type.enum';
 export { ErrorType } from './enums/error-type.enum';
+export { SortingType } from './enums/sorting-type.enum';
 export { ActionResponse } from './models/action-response.interface';
 export { ErrorMessage } from './models/error-message.interface';
 export { HttpService } from './services/http.service';

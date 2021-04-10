ffb96f3c7c8ca160e90a0f1feff58db91325c384
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2021-04-11 01:37:49 +0200

    Enhanced Http Auth headers usage

--- cs-common/src/enums/auth-type.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum AuthType{
-    None,
-    Basic,
-    JWT
-}
--- cs-common/src/index.ts
+++ cs-common/src/index.ts
@@ -1,6 +1,6 @@
 export { ActionResponseType } from './enums/action-response-type.enum';
 export { ErrorType } from './enums/error-type.enum';
-export { AuthType } from './enums/auth-type.enum';
+export { BasicAuthentication } from './models/basic-authentication.class';
 export { SortingType } from './enums/sorting-type.enum';
 export { ActionResponse } from './models/action-response.interface';
 export { ErrorMessage } from './models/error-message.interface';
